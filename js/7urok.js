0e87dd0e007c7432eada1644a1711df4f9921929
Abzal <abzik1177@gmail.com>
2020-07-29 16:26:08 +0500

    first commit

---
+++ js/7urok.js
@@ -0,0 +1,59 @@
+"use sctrict";
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//savol berish
+//const answer = prompt("Siz 18 yoshga to'lganmisiz?", "18");
+//console.log(typeof(answer + 5);
+
+//const answers = [];
+/*
+answers[0] = prompt("What is Your name?", " ");
+answers[1] = prompt("What is Your surname?", " ");
+answers[2] = prompt("How old are you?", " ");
+
+document.write(answers[1]);
+*/
+
+/* 8 dars
+const category = 'toys';
+
+console.log('https://wtma.uz/' + category);
+
+const user = 'ivan';
+
+alert(`Привет ${user}`);
+
+*/
+
+// 9 dars
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(8%3); // qoldiq chiqarish
+
+console.log(2*4 == 8); // togrimi - true
+console.log(2*4 === '8'); // togrimi - false
+console.log(2*4 === !8); // neraven !
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked && isClose); // VA - hammasi togri bolishi kerak
+console.log(isChecked || !isClose); // YOKI - bittasi togri bolsa yetarli
+
+// 10 dars
+
---
+++ js/script.js
@@ -0,0 +1,36 @@
+//Ozgaruvchab let
+let number = 5;
+
+//Ozgarmas const
+const leftBorderSize = 1;
+
+//
+number = 10;
+console.log(number);
+
+//
+var nameIs = 'Abzal';
+
+console.log(nameIs);
+
+/// OBYELTI
+
+// klytuch i znacheniya
+const obj = {
+    Surname: 'Sabitov',
+    age: 25,
+    isMarried: false
+    klyuch: 'znacheniya',
+};
+
+
+console.log(obj.Surname);
+console.log(obj["Surname"]);
+
+// toka znacheniya
+//              0               1           2       3       4       5   6
+let arr = ['Znacheniya', 'plum.png', 'orange.jpg', 6, 'apple.com', {}, [],];
+console.log(arr[2]);
+
+// urok 7
+ 
